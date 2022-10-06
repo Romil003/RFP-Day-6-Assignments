8fb9ddbd2a090c576f862e40692a82e25a6ade67
Romil003 <romilghadge33@gmail.com>
2022-10-06 14:13:16 +0530

    Added Check Equal String

---
+++ RFP Day 1 Assignment/src/CheckEqualString.java
@@ -0,0 +1,12 @@
+public class CheckEqualString {
+    public static void main(String[] args) {
+        String c = "one" ;
+        String d = "two" ;
+        if (c == d) {
+            System.out.println("Strings are equal");
+        }
+        else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
